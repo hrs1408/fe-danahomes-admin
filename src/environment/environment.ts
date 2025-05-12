f8e896b3b2b518f27d3b7d216f7905cdaed24519
hieunph <hieunph@vnpt.vn>
2025-05-12 18:09:30 +0700

    cty

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000/',
+  apiUrl: 'https://hieunph.online/',
   tinymceApiKey: 'o5p2i4ta234xgw9rqc9eganazv4afvva6h1a3olbtus5zx4s'
 };
